05ccf569ae1e234f60cae6cc98ae22b6aa9e2f03
sobhan9012 <sobhansahoo2000@gmail.com>
2024-06-27 15:49:00 +0530

    start making the airbnb project

---
+++ src/app/components/Container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+    children: React.ReactNode;
+    
+}
+
+
+import React from 'react'
+
+function Container() {
+  return (
+    <div>Container</div>
+  )
+}
+
+export default Container
